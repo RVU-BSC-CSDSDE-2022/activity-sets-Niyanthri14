dfebaf15d6562719d8c8064d2fb412f3f484a732
Niyanthri R  R <115770129+Niyanthri14@users.noreply.github.com>
2022-12-01 09:39:03 +0000

    solve

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,26 +1,29 @@
 //Write a C program to add two numbers using **pass by reference**
-#include<stdio.h>
+#include <stdio.h>
 int input();
 void add(int a, int b, int *sum);
 void output(int a, int b, int sum);
-int main()
-{
-  input();
-}
-int input()
-{
-  int a,b;
-  printf("Enter the numbers: ");
-  scanf("%d%d",&a,&b);
-  add(a,b);
-}
-int add(int a, int b)
-{
-  int *sum = 0;
-  sum = a + b;
+
+int main(){
+  int a = input();
+  int b = input();
+  int sum;
+  add (a,b,&sum);
   output(a,b,sum);
 }
-void output(int a, int b, int sum)
+
+int input(){
+  int x;
+  printf("Enter the number\n");
+  scanf("%d", &x);
+  return(x);
+}
+
+void add(int a, int b,int *sum)
 {
-  printf("The sum of %d and %d is %d", a, b, sum);
+  *sum=a+b;
+}
+
+void output(int a,int b, int sum){
+  printf("The sum of %d and %d is %d\n",a,b,sum);
 }
--- set01/problem05.c
+++ set01/problem05.c
@@ -1,13 +1,36 @@
 //Write a C program to compare three numbers using **pass by value**
-#include<stdio.h>
+#include <stdio.h>
 int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
-int main()
-{
-  input()
-}
-int input()
-{
+
+int main(){
+  int a,b,c,l;
+  a = input();
+  b = input();
+  c = input();
+  l = compare(a,b,c);
+  output(a,b,c,l);
   
 }
+
+int input(){
+  int x;
+  printf("enter a number\n");
+  scanf("%d",&x);
+  return(x);  
+}
+
+int compare(int a, int b, int c)
+{
+	if (  a>=b && a>=c) 
+		return a;
+	if ( b>=c)
+		return b;
+	return c;
+}
+
+
+void output(int a, int b, int c, int largest){
+  printf("the largest of %d,%d and %d is %d" ,a,b,c,largest); 
+} 
