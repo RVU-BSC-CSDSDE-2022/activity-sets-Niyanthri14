0a56b3bfa4a971358a8331041622cf1c55ddbabf
Niyanthri R  R <115770129+Niyanthri14@users.noreply.github.com>
2022-12-14 08:46:10 +0000

    solve

--- set01/README.md
+++ set01/README.md
@@ -1,4 +1,4 @@
-# Set 1
+23# Set 1
 
 1. Write a C program to print your name.
 
--- set02/problem02.c
+++ set02/problem02.c
@@ -1,5 +1,33 @@
 //Write a program to find if a triangle is scalene.
 #include<stdio.h>
-#include<conio.h>
-void main()
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+
+int main()
+{
+  int a,b,c, isscalene;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  isscalene=check_scalene(a,b,c);
+  output(a,b,c,isscalene);
+}
+
+int input_side()
+{
+  int n;
+  printf("Enter number of sides");
+  scanf("%d",&n);
+  return n;
+}
+
+int check_scalene(int a, int b, int c)
+{
+  int isscalene;
+  if (a != b && b != c && c != a)
+    return isscalene;
+  else if (a==b && b==c)
+return isscalene;
+}
 
